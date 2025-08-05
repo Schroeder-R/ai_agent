fed9cf3d92d8d39805db26039cf7bf67fafda80c
Schroeder-R <Schroeder357@gmail.com>
2025-08-04 21:18:02 -0500

    add write_file from lesson

---
+++ functions/write_file.py
@@ -0,0 +1,19 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(os.path.dirname(target_file)):
+        try:
+            os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        except Exception as e:
+            return f'Error: Could not create parent directory for "{file_path}": {e}'
+    try:
+        with open(target_file, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: Could not write to "{file_path}": {e}'
+        
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
@@ -25,23 +26,38 @@ def test():
     # print(result)
     # print("")
 
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' file:")
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for 'main.py' file:")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for 'pkg/calculator.py' file:")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for '/bin/cat' file:")
+    # print(result)
+    # print("")
+
+    # result = get_file_content("calculator", "pkg/does_not_exist.py")
+    # print("Result for 'pkg/does_not_exist.py' file:")
+    # print(result)
+    # print("")
+
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(f"Result for writing to 'lorem.txt':")
     print(result)
     print("")
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py' file:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(f"Result for writing to 'pkg/morelorem.txt':")
     print(result)
     print("")
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat' file:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for 'pkg/does_not_exist.py' file:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(f"Result for writing to '/tmp/temp.txt':")
     print(result)
     print("")
 
