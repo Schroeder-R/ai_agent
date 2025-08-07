b1cbea5c1c6369d4fea786cc57719c76cd7f2278
Schroeder-R <Schroeder357@gmail.com>
2025-08-06 19:03:57 -0500

    add the run_python_file function

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def test():
@@ -46,18 +47,43 @@ def test():
     # print(result)
     # print("")
 
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(f"Result for writing to 'lorem.txt':")
+    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # print(f"Result for writing to 'lorem.txt':")
+    # print(result)
+    # print("")
+
+    # result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    # print(f"Result for writing to 'pkg/morelorem.txt':")
+    # print(result)
+    # print("")
+
+    # result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    # print(f"Result for writing to '/tmp/temp.txt':")
+    # print(result)
+    # print("")
+
+    result = run_python_file("calculator", "main.py")
+    print(f"Result for running main.py")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(f"Result for writing to 'pkg/morelorem.txt':")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
+    print(f"Result for running main.py with args ['3 + 5']")
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(f"Result for writing to '/tmp/temp.txt':")
+    result = run_python_file("calculator", "tests.py")
+    print(f'Result for running tests.py')
+    print(result)
+    print("")
+
+    result =run_python_file("calculator", "../main.py")
+    print(f'Result for running../main.py')
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(f'Result for running nonexistent.py')
     print(result)
     print("")
 
